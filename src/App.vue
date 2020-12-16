99e65b151668cd8190db3ed8b8a4c9962b7683bd
Pablo Diaz <pabloandi@gmail.com>
2020-12-16 11:37:43 -0500

    ep15: named slots in a nutshell ~ completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld>
+      vaya vaya perro
+    </HelloWorld> -->
+    <Modal>
+      <template slot="title">Titulo para el modal</template>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, animi.
+      
+    </Modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="modalClass">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+          <header class="modal-card-head">
+              <p class="modal-card-title">
+                  <slot name="title"></slot>
+              </p>
+              <button class="delete" @click="closeModal"></button>
+          </header>
+          <section class="modal-card-body">
+              <slot>
+                  Default content here
+              </slot>
+          </section>
+          <footer class="modal-card-foot">
+              <slot name="footer">
+                  <a href="#" class="button is-primary">Save changes</a>    
+                  <a href="#" class="button" @click="closeModal">Cancel</a>
+              </slot>
+          </footer>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    data() {
+        return {
+            close: false
+        }
+    },
+    methods: {
+        closeModal(){
+            this.close = true;
+        }
+    },
+    computed: {
+        modalClass(){
+            return (this.close) ? 'modal' : 'modal is-active'
+        }
+    },
+}
+</script>
+
+<style scoped>
+    @import url('https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css');
+</style>
